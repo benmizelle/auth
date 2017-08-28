c96b4c8d4d7fd1712a15f30e797c2392cb29b72b
H Cindymizelle M <cindymizelle@iMac.local>
2017-08-27 22:23:26 -0400

    save

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,8 +1,23 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function (req, res, next) {
+    res.send({ token: tokenForUser(req.user) });
+}
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
+
+    if (!email || !password) {
+        return res.status(422).send({ error: 'You must provide a user name and password'});
+    }
 
     // Checks for an existing user name to prevent duplicates
     User.findOne({ email: email}, function(err, existingUser) {
@@ -23,7 +38,7 @@ exports.signup = function(req, res, next) {
             if (err) { return next(err); }
 
             // Respond to request indicating the user was successfully created
-            res.json({ success: true });
+            res.json({ token: tokenForUser(user) });
         });
 
     });
